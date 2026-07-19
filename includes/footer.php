4465d19ed1960fc76c7479aa5cc74c5c847ae8cf
Noman <212067060+luminancexe@users.noreply.github.com>
2026-07-19 22:18:28 +0600

    Updated upload of GameDock

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
     <footer>
         <div class="container">
             <div>
-                <a href="#">About</a>
+                <a href="about.php">About</a>
+                <a href="faq.php">FAQ</a>
                 <a href="contact.php">Contact</a>
                 <a href="privacy.php">Privacy Policy</a>
                 <a href="terms.php">Terms and Conditions</a>
